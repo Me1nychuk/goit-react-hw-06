230f96f07f17c902b82bdb6312ab6f74b11f8ba0
Sviatoslav <113264589+Me1nychuk@users.noreply.github.com>
2024-03-25 00:28:35 +0200

    Add fix for Non-Serializable Data

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,7 +1,16 @@
 import { configureStore } from "@reduxjs/toolkit"; 
 import { contactsReducer } from "./contactsSlice";
 import { filterReducer } from "./filtersSlice";
-import { persistStore, persistReducer } from 'redux-persist'
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from 'redux-persist'
 import storage from 'redux-persist/lib/storage'
 
 
@@ -17,7 +26,14 @@ export const store = configureStore({
   reducer: {
     contacts: persistedReducer,
     filters: filterReducer,
+    
   },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
 });
 
 export const persistor = persistStore(store);
