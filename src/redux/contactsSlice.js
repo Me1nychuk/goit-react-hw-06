ed69c7e41454a4c9e9b587266215730c44f33e94
Sviatoslav <113264589+Me1nychuk@users.noreply.github.com>
2024-03-25 21:23:54 +0200

    Add fix for contact's reducer - deleteContact

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -27,7 +27,7 @@ const contactsSlice = createSlice({
             },
         },
         deleteContact: (state, action) => {
-            const index = state.items.findIndex(contact => contact.id === action.payload.id);
+            const index = state.items.findIndex(contact => contact.id === action.payload);
             state.items.splice(index, 1);
         },
         
